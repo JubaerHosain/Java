2ff78491beb92d46c2cb2cc283fd3c3e55841bdc
henry226 <henry5581984@gmail.com>
2018-10-07 19:55:56 -0400

    sum all the elements in array

---
+++ Chapter7 (Arrays and ArrayLists)/SumArray.java
@@ -0,0 +1,15 @@
+package Chapter7;
+
+public class SumArray {
+	public static void main(String[] args) {
+		int[] array = {87, 68, 94, 100, 83, 78, 85, 91, 76, 87};
+		int total = 0;
+		
+		// add each element's value to total
+		for(int i=0; i<array.length; i++) {
+			total += array[i];
+		}
+		
+		System.out.println("Total of array elements: " + total);
+	}
+}
