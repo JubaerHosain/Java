aff0d28ad237cc66c68d1a424574d37e753f96e6
henry226 <henry5581984@gmail.com>
2018-10-07 20:05:53 -0400

    Bar chart printing program

---
+++ Chapter7 (Arrays and ArrayLists)/BarChart.java
@@ -0,0 +1,29 @@
+package Chapter7;
+
+public class BarChart {
+
+	public static void main(String[] args) {
+		int[] array = {0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1};
+		
+		System.out.println("Grade distribution:");
+		
+		//for each array element, output a bar of the chart
+		for (int counter = 0; counter < array.length; counter ++) {
+			//output bar label ("00-99: ", ... "99-99: ", "100: ")
+			if(counter == 10) {
+				System.out.printf("%5d: ", 100);
+			}
+			else {
+				System.out.print(counter*10 + "-" + (counter*10+9) +": ");
+			}
+			// print bar of asterisks
+			for(int stars=0; stars<array[counter]; stars++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
