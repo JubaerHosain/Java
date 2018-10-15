e41536462c7f06a04ef6343fe5926cc8808242de
henry226 <henry5581984@gmail.com>
2018-10-15 18:35:30 -0400

    static variable program

---
+++ Chapter8 (Classes and Objects)/StaticEmployee/Employee.java
@@ -0,0 +1,36 @@
+package Chapter8.StaticEmployee;
+
+// static variable used to maintain a count of the number of Employee 
+//Objects in memory
+public class Employee {
+	private static int count = 0;
+	private String firstName;
+	private String lastName;
+	
+	//initialize Employee, and 1 to static count and output String 
+	// indicating that constructor was called
+	public Employee(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		
+		++count; // increment static count of employees
+		System.out.printf("Employee constructor: %s %s; count = %d%n", firstName, lastName, count);
+	}
+	
+	// static method to get static count value
+	public static int getCount() {
+		return count;
+	}
+
+	
+	// get first name
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	// get last name
+	public String getLastName() {
+		return lastName;
+	}
+
+}
