0e6caa1498b227f3ecc1daa73c9d4f35ebd1c6f5
henry226 <henry5581984@gmail.com>
2018-11-18 16:06:13 -0500

    sort

---
+++ Chapter16 (Generic Collections)/Sort1.java
@@ -0,0 +1,19 @@
+package Chapter16;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort1 {
+	public static void main(String[] args) {
+		String[] suits = {"hearts", "Diamonds", "Clubs", "Spades"};
+		
+		// Create and display a list containing the suits array elements
+		List<String> list = Arrays.asList(suits);
+		System.out.printf("Unsorted array elements: %s%n", list);
+		
+		Collections.sort(list);
+		System.out.printf("\nSorted array elements: %s%n", list);
+	}
+
+}
