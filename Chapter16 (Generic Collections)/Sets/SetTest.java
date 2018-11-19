58f34a71aedefe6561ac6d4fefd6a3e76cecb570
henry226 <henry5581984@gmail.com>
2018-11-19 12:49:38 -0500

    remove set import and add tree set

--- Chapter16 (Generic Collections)/Sets/SetTest.java
+++ Chapter16 (Generic Collections)/Sets/SetTest.java
@@ -2,7 +2,6 @@ package Chapter16.Sets;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
