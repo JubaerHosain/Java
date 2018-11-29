b264923777e5edcb3c96fd41bc82f6c4ece98aaf
Henry Li <henry5581984@gmail.com>
2018-11-29 12:22:22 -0500

    chapter11 exception handling

---
+++ Chapter11 (Exception Handling)/DivideByZeroNoExceptionHandling.java
@@ -0,0 +1,23 @@
+package Ch11;
+import java.util.Scanner;
+
+// Integer division without exception handling
+public class DivideByZeroNoExceptionHandling {
+	// demonstrates throwing an exception when a divide-by-zero ocurs
+	public static int quotient(int numerator, int denominator) {
+		return numerator / denominator; // possibe division by zero
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Please enter an integer numerator: ");
+		int numerator = scanner.nextInt();
+		System.out.print("Please enter an integer denominator: ");
+		int denominator = scanner.nextInt();
+		
+		int result = quotient(numerator, denominator);
+		System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+	}
+
+}
