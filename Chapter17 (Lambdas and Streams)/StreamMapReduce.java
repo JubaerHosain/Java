cb425fe423b2d512206c73bc52465e99eb41244e
Henry <henry5581984@gmail.com>
2018-12-03 13:04:45 -0500

    Update StreamMapReduce.java
    fix line break

--- Chapter17 (Lambdas and Streams)/StreamMapReduce.java
+++ Chapter17 (Lambdas and Streams)/StreamMapReduce.java
@@ -8,9 +8,9 @@ public class StreamMapReduce {
 		// sum the even integers from 2 through 20
 		System.out.printf("Sum of the even ints from 2 through 20 is: %d%n", 
 				IntStream
-				.rangeClosed(1, 10)				// 1...10
+				.rangeClosed(1, 10)		// 1...10
 				.map((int x) -> {return x*2;})	// multiply by 2
-				.sum());						// sum
+				.sum());			// sum
 	}
 
 }
