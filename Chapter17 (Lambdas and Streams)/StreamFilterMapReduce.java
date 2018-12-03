1e0f52498f5babac9d5209da30856b7e7feb3f57
Henry <henry5581984@gmail.com>
2018-12-03 13:05:30 -0500

    Update StreamFilterMapReduce.java
    line break fix

--- Chapter17 (Lambdas and Streams)/StreamFilterMapReduce.java
+++ Chapter17 (Lambdas and Streams)/StreamFilterMapReduce.java
@@ -7,10 +7,11 @@ public class StreamFilterMapReduce {
 	public static void main(String[] args) {
 		// sum the even integers from 2 through 20
 		System.out.printf("Sum of the triple of the even integers from 2 through 20 is: %d%n", 
-				IntStream.rangeClosed(1, 10)			
-						 .filter(x -> x%2 == 0)
-						 .map(x -> x*3)
-						 .sum());						
+			IntStream
+			.rangeClosed(1, 10)			
+			.filter(x -> x%2 == 0)
+			.map(x -> x*3)
+			.sum());						
 	}
 
 }
