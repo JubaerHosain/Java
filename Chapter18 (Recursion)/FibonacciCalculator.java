9192c51bd6051059390684886990c790d858822e
Henry Li <henry5581984@gmail.com>
2019-01-28 18:31:58 -0500

    fib calculator

---
+++ Chapter18 (Recursion)/FibonacciCalculator.java
@@ -0,0 +1,23 @@
+package Ch18;
+
+import java.math.BigInteger;
+
+public class FibonacciCalculator {
+	private static BigInteger TWO = BigInteger.valueOf(2);
+
+	public static void main(String[] args) {
+		// displays the fibonacci values from 0-40
+		for(int counter=0; counter<=40; counter++) {
+			System.out.printf("Fibonacci of %d is: %d%n", counter, fibonacci(BigInteger.valueOf(counter)));
+		}
+	}
+	
+	public static BigInteger fibonacci(BigInteger number) {
+		if(number.equals(BigInteger.ZERO) || number.equals(BigInteger.ONE)) {	// base cases
+			return number;
+		}
+		else 
+			return fibonacci(number.subtract(BigInteger.ONE)).add(fibonacci(number.subtract(TWO)));
+	}
+
+}
