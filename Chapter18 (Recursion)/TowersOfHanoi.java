4591d273de3e369e23b96c9403c327fa4ab9fc9a
Henry Li <henry5581984@gmail.com>
2019-01-29 00:11:25 -0500

    towers of Hanoi

---
+++ Chapter18 (Recursion)/TowersOfHanoi.java
@@ -0,0 +1,37 @@
+package Ch18;
+
+// Towers of Hanoi solution with a recursive method.
+public class TowersOfHanoi {
+
+	public static void main(String[] args) {
+		int startPeg = 1; // value 1 used to indicate startPeg in output
+		int endPeg = 3; // value 3 used to indicate endPeg in output
+		int tempPeg = 2; // value 2 used to indicate tempPeg in output
+		int totalDisks = 3; // number of disks
+		
+		// initial nonrecursive call: move all disks
+		solveTowers(totalDisks, startPeg, endPeg, tempPeg);
+	}
+	
+	// recursively move disks between towers
+	public static void solveTowers(int disks, int sourcePeg, int destinationPeg, int tempPeg) {
+		System.out.println("here: " + disks + sourcePeg + destinationPeg + tempPeg);
+		// base case -- only one disk to move
+		if(disks == 1) {
+			//System.out.print("A)" + disks + sourcePeg + destinationPeg + tempPeg);
+			System.out.printf("%n%d --> %d", sourcePeg, destinationPeg);
+			return;
+		}
+		
+		// recursion step -- move (disk - 1) disks from sourcePeg to tempPeg using destinationPeg
+		solveTowers(disks - 1, sourcePeg, tempPeg, destinationPeg);
+		
+		//System.out.printf("%nB)");
+		// move last disk from sourcePeg to destinationPeg
+		System.out.printf("%n%d --> %d%n", sourcePeg, destinationPeg);
+		
+		// move (disk - 1) disks from tempPeg to destinationPeg
+		solveTowers(disks - 1, tempPeg, destinationPeg, sourcePeg);
+	}
+
+}
