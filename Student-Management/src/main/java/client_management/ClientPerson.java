c908e8037851893f0f386c0c182fc3c3aabacf1c
jubaer <jubaerhosain1119@gmail.com>
2022-04-22 02:46:55 +0600

    Deleted Student-Management

--- Student-Management/src/main/java/client_management/ClientPerson.java
+++
@@ -1,9 +0,0 @@
-package client_management;
-
-import java.util.List;
-
-public interface ClientPerson {
-    public <Type> boolean signUp(Type object);
-    public <Type> boolean logIn(Type object);
-    public List<Object> getInformation();
-}
